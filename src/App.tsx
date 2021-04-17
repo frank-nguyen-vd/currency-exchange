fb422193a6b9ef2baf0193db93a4bfdfd75d2a27
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-04-17 11:50:45 +0800

    Implement buttons +1, +5, +10 and +100

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Button(props : any) {
-    return (
-        <button onClick={ props.onClickFunc }>
-            + 1
-        </button>
-    );
+function Button(props: any) 
+{
+    const handleClick = () => props.onClickFunc(props.increment);
+    return <button onClick={handleClick }> +{props.increment} </button>;
 }
 
-function Display(props : any) {
-    return (
-        <div>{ props.message }</div>
-    )
+function Display(props: any) {
+    return <div>{props.message}</div>;
 }
 
 function App() {
     // eslint-disable-next-line
     const [counter, setCounter] = useState(0);
-    const incCounter = () => setCounter(counter + 1);
+    const incCounter = (val: number) => setCounter(counter + val);
 
     return (
-        <div>            
-            <Button onClickFunc={ incCounter }/>
-            <Display message={ counter } />
+        <div>
+            <Button onClickFunc={incCounter} increment={1} />
+            <Button onClickFunc={incCounter} increment={5} />
+            <Button onClickFunc={incCounter} increment={10} />
+            <Button onClickFunc={incCounter} increment={100} />
+            <Display message={counter} />
         </div>
     );
 }
