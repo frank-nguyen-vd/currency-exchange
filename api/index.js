0e32cc91caa1d91f81c18a48c40c4646c1ed25f6
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-04-17 17:21:32 +0800

    Display latest rates for 6 currencies with base currency being SGD

--- api/index.js
+++ api/index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const https = require("https");
+
 require("dotenv").config();
+
+const cors = require("cors");
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200
+};
 
 const endpoint = "latest";
 const api_key = process.env.API_KEY;
 const url = "https://api.currencyscoop.com/v1/";
 
-app.get("/rates", (req, res) => {
+app.get("/rates", cors(corsOptions), (req, res) => {
     base = req.query.base;
     https.get(
         url + endpoint + "?api_key=" + api_key + "&base=" + base,
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,34 +16,70 @@ function CurrencyExchangeCard(props: any) {
     );
 }
 
-function CurrencyExchangeGrid() {
-    return (
-        <div>
-            <Row gutter={[16, 24]}>
-                <Col className="gutter-row" span={6}>
-                    <CurrencyExchangeCard
-                        currency="USD"
-                        rate={0.72}
-                        currency_desc="US Dollar"
-                    />
-                </Col>
-                <Col className="gutter-row" span={6}>
-                    <CurrencyExchangeCard
-                        currency="AUD"
-                        rate={1.02}
-                        currency_desc="Australian Dollar"
-                    />
-                </Col>
-                <Col className="gutter-row" span={6}>
-                    <CurrencyExchangeCard
-                        currency="CAD"
-                        rate={1.32}
-                        currency_desc="Canadian Dollar"
-                    />
-                </Col>
-            </Row>
-        </div>
-    );
+class CurrencyExchangeGrid extends React.Component {
+    state = {
+        data: { USD: 0, AUD: 0, CAD: 0, VND: 0, MYR: 0, PHP: 0 }
+    };
+
+    componentDidMount() {
+        fetch("http://localhost:3000/rates?base=SGD")
+            .then((response) => response.json())
+            .then((result) => this.setState({ data: result }));
+    }
+
+    render() {
+        const data = this.state.data;
+        return (
+            <div>
+                <Row gutter={[16, 24]}>
+                    <Col className="gutter-row" span={6}>
+                        <CurrencyExchangeCard
+                            currency="USD"
+                            rate={data.USD}
+                            currency_desc="US Dollar"
+                        />
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <CurrencyExchangeCard
+                            currency="AUD"
+                            rate={data.AUD}
+                            currency_desc="Australian Dollar"
+                        />
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <CurrencyExchangeCard
+                            currency="CAD"
+                            rate={data.CAD}
+                            currency_desc="Canadian Dollar"
+                        />
+                    </Col>
+                </Row>
+                <Row gutter={[16, 24]}>
+                    <Col className="gutter-row" span={6}>
+                        <CurrencyExchangeCard
+                            currency="VND"
+                            rate={data.VND}
+                            currency_desc="Vietnamese Dong"
+                        />
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <CurrencyExchangeCard
+                            currency="MYR"
+                            rate={data.MYR}
+                            currency_desc="Malaysian Ringgit"
+                        />
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <CurrencyExchangeCard
+                            currency="PHP"
+                            rate={data.PHP}
+                            currency_desc="Philippine Peso"
+                        />
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 function App() {
